de664cea66008ada3762f4dc23e48d390549ea28
Juferoga <juancholoco2000.61@gmail.com>
2024-10-24 11:20:20 -0500

    feat: terrafomr

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,24 +1,5 @@
-resource "aws_s3_bucket" "example" {
-  bucket = "angular-clean-architecture"
-  versioning {
-    enabled = true
-  }
-}
-
-resource "aws_s3_bucket_policy" "example" {
-  bucket = aws_s3_bucket.example.id
-  policy = data.aws_iam_policy_document.bucket_policy.json
-}
-
-data "aws_iam_policy_document" "bucket_policy" {
-  statement {
-    sid = "AllowPublicRead"
-    effect = "Allow"
-    principals {
-      type        = "AWS"
-      identifiers = ["*"]
-    }
-    actions = ["s3:GetObject"]
-    resources = ["arn:aws:s3:::angular-clean-architecture/*"]
-  }
+resource "aws_ssm_parameter" "foo" {
+  name  = "foo"
+  type  = "String"
+  value = "barr"
 }
